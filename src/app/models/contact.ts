cb26513c83f7dfc5f8aec0cdf4a0c646032dce0a
Kennedy Sigauke <KSigauke@jhb.dvt.co.za>
2020-03-23 10:56:16 +0200

    Initialize project

---
+++ src/app/models/contact.ts
@@ -0,0 +1,14 @@
+import {Title} from './title';
+import {Gender} from './gender';
+
+export class Contact {
+  name: string;
+  firstName: string;
+  middleName: string;
+  lastName: string;
+  gender: Gender;
+  birthDate: Date;
+  title: Title;
+  phoneNumber: string;
+  emailAddress: string;
+}
